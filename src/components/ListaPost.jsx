942c8fcdf1298d6c648f5410e0419acab63f3e19
João Araújo <joaogabriel-al@hotmail.com>
2022-04-29 17:13:21 -0300

    03 - 04.Exibindo os posts

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import {busca} from "../api/api"
 
-const ListaPost = () => {
+const ListaPost = ({url}) => {
+
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    busca(url, setPosts)
+  }, []);
+  
   return(
     <section className="posts container">
-
+      {
+        posts.map((post) => {
+          return(
+            <Link to="#" className={`cartao-post cartao-post--${post.categoria}`}>
+            <article key={post.id}>
+              <h3 className="cartao-post__titulo">{post.title}</h3>
+              <p className="cartao-post__meta">{post.metadescription}</p>
+            </article>
+          </Link>
+          )
+        })
+      }
     </section>
   )
 }
