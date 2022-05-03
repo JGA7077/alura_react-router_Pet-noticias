a32f2e545d829d5166b6e15ba6b0d0694d63a100
João Araújo <joaogabriel-al@hotmail.com>
2022-05-03 16:21:12 -0300

    05 - 04.Criando SubCategoria

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, useParams, useRouteMatch } from "react-router-dom";
+import { busca } from "../api/api";
 import "../assets/css/blog.css";
 import ListaCategorias from "../components/ListaCategorias";
 import ListaPost from "../components/ListaPost";
@@ -7,6 +8,12 @@ import ListaPost from "../components/ListaPost";
 const Categoria = () => {
   const { id } = useParams();
   const { path } = useRouteMatch();
+  const [subcategorias, setsubcategorias] = useState([]);
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setsubcategorias(categoria.subcategorias)
+    })
+  }, [id]);
   return (
     <>
       <div className="container">
