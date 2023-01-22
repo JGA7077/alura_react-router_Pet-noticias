ccbf5ccbd832018e1fa7966d9b911880722fff58
João Gabriel <joaogabriel-al@hotmail.com>
2023-01-21 21:55:05 -0300

    react-router downgrade

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./assets/css/base/base.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
@@ -13,13 +13,23 @@ function App() {
   return (
     <Router>
       <Cabecalho />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/categoria/:id" element={<Categoria />} />
-        <Route path="/posts/:id" element={<Post />} />
-        <Route element={<Pagina404 />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/sobre">
+          <Sobre />
+        </Route>
+        <Route path="/categoria/:id">
+          <Categoria />
+        </Route>
+        <Route path="/posts/:id">
+          <Post />
+        </Route>
+        <Route>
+          <Pagina404 />
+        </Route>
+      </Switch>
     </Router>
   );
 }
